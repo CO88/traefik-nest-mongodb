e866d85811dc8615dd7573f005b2eb8891fc8e49
cobb <121212112@naver.com>
2021-05-24 23:35:36 +0900

    mongodb auth 관련 이슈 처리중

--- deploy/mongo-init.sh
+++ deploy/mongo-init.sh
@@ -1,12 +1,8 @@
-
-mongo <<EOF
+mongo -u "${MONGO_INITDB_ROOT_USERNAME}" -p "${MONGO_INITDB_ROOT_PASSWORD}" --authenticationDatabase "admin" <<EOF
 use admin
-db.auth('${MONGO_INITDB_ROOT_USERNAME}', '${MONGO_INITDB_ROOT_PASSWORD}')
-
-use '${MONGO_INITDB_DATABASE}'
 db.createUser({
-    user: '${MONGO_INITDB_ROOT_USERNAME}',
-    pwd: '${MONGO_INITDB_ROOT_PASSWORD}',
+    user: '${MONGO_DATABASE_USERNAME}',
+    pwd: '${MONGO_DATABASE_PASSWORD}',
     roles: [{ role: 'readWrite', db: '${MONGO_INITDB_DATABASE}' }],
 });
 EOF
